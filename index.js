2ae3eac13d9483a73cfeab4bb99e100d19c7f452
andyexeter <palmer.andy@gmail.com>
2026-07-21 11:31:30 +0100

    Use require.resolve for importing twig plugin

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
   trailingComma: "none",
   tabWidth: 4,
   printWidth: 250,
-  plugins: ["@destination/prettier-plugin-twig"]
+  plugins: [require.resolve("@destination/prettier-plugin-twig")]
 };
